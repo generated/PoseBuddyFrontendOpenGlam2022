1121e09242398ff5865d82c333467f13e8fd5cbd
Ficht Hämmerli <ficht@ficht.ch>
2022-11-05 12:27:11 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'paintPoserFrontend';
+  @ViewChild("video")
+  public video: ElementRef;
+
+  @ViewChild("canvas")
+  public canvas: ElementRef;
+
+  public captures: Array<any>;
+
+  public constructor() {
+      this.captures = [];
+  }
+
+  public ngOnInit() { }
+
+  public ngAfterViewInit() {
+      if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+          navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+              this.video.nativeElement.srcObject = stream;
+              this.video.nativeElement.play();
+          });
+      }
+  }
+
+  public capture() {
+      // var context = this.canvas.nativeElement.getContext("2d").drawImage(this.video.nativeElement, 0, 0, 640, 480);
+      console.log(this.canvas.nativeElement.toDataURL("image/png"));
+      this.captures.push(this.canvas.nativeElement.toDataURL("image/png"));
+  }
 }
